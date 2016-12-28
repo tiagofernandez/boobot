395e1adf970f9b04f483e5c5404fd7966221df79
Tiago Fernandez <tiago.fernandez@gmail.com>
2016-12-28 16:03:22 +0100

    Use cache with expiration of 12h for events

--- src/github.com/tiagofernandez/goatbot/events.go
+++ src/github.com/tiagofernandez/goatbot/events.go
@@ -1,7 +1,12 @@
 package goatbot
 
-// TODO Use Redis with 12 hours of expiration
-var kvs = make(map[string]map[string]bool)
+import (
+	"github.com/patrickmn/go-cache"
+	"time"
+)
+
+// Cache with default expiration of 12 hours, purging every 10 minutes
+var kvs = cache.New(12*time.Hour, 10*time.Minute)
 
 func Confirm(group string, user string) {
 	set(group, user, true)
@@ -15,19 +20,27 @@ func Cancel(group string, user string) {
 
 func ConfirmedList(group string) []string {
 	keys := []string{}
-	for k := range kvs[group] {
-		keys = append(keys, k)
+	data, found := kvs.Get(group)
+	if found {
+		for k := range data.(map[string]bool) {
+			keys = append(keys, k)
+		}
 	}
 	return keys
 }
 
 func set(group string, user string, confirmed bool) {
-	if kvs[group] == nil {
-		kvs[group] = make(map[string]bool)
+	data, found := kvs.Get(group)
+	var values map[string]bool
+	if found {
+		values = data.(map[string]bool)
+	} else {
+		values = make(map[string]bool)
 	}
 	if confirmed {
-		kvs[group][user] = confirmed
+		values[user] = confirmed
 	} else {
-		delete(kvs[group], user)
+		delete(values, user)
 	}
+	kvs.Set(group, values, cache.DefaultExpiration)
 }
